3c5657849e97daff45c00de0bf8c15e3ef0a049f
jinguotao <jgtshiwo123@163.com>
2020-03-09 23:28:42 +0800

    first commit

---
+++ gateway_zull/src/main/java/com/offcn/gateway_zull/GatewayZullApplication.java
@@ -0,0 +1,14 @@
+package com.offcn.gateway_zull;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@
+public class GatewayZullApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayZullApplication.class, args);
+    }
+
+}
---
+++ gateway_zull/src/test/java/com/offcn/gateway_zull/GatewayZullApplicationTests.java
@@ -0,0 +1,13 @@
+package com.offcn.gateway_zull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayZullApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
